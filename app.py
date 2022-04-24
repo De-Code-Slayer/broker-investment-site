46f87287540fb3e18cee45e96f103dd64bacdafc
emeka <chukwujapheth232@gmail.com>
2022-04-24 20:09:54 +0300

    update

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from webapp import  create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80, threaded=True)
+    app.run(debug=False, host="0.0.0.0", port=80, threaded=True)

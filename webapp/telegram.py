67aeabb5942e0b2d7e247dcea9b1bc0c601c3bd3
emeka <chukwujapheth232@gmail.com>
2022-11-27 21:53:38 +0200

    .

--- webapp/telegram.py
+++ webapp/telegram.py
@@ -6,4 +6,5 @@ def send_msg(text):
    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
    results = requests.get(url_req)
    print(results.json())
+   return results
 
